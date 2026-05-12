c04ee8a51c35e4331e2821c3c99c33c499a6dbd9
Olajuwon47 <lawal.olajuwonwaheed@gmail.com>
2026-05-12 18:44:04 +0100

    Done

--- vite.config.ts
+++ vite.config.ts
@@ -1,42 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
-import { fileURLToPath } from 'url';
 import tailwindcss from '@tailwindcss/vite';
-// import { visualizer } from 'rollup-plugin-visualizer';
-
-const srcPath = fileURLToPath(new URL('./src', import.meta.url));
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    // visualizer({
-    //   filename: 'bundle-report.html',
-    //   open: true, // Automatically open after build
-    //   gzipSize: true,
-    //   brotliSize: true,
-    //   template: 'treemap', // nicer layout
-    // }),
-  ],
-
-  resolve: {
-    alias: {
-      '@': srcPath,
-    },
-  },
-
+  plugins: [react(), tailwindcss()],
   build: {
     sourcemap: false,
-    rollupOptions: {
-      output: {
-        // 💡 This breaks large libraries into smaller chunks
-        manualChunks: {
-          react: ['react', 'react-dom'],
-          vendor: ['axios', 'react-router-dom', 'recharts'],
-        },
-      },
-    },
-    chunkSizeWarningLimit: 1000, // optional: raises limit from 500kB to 1MB
+    chunkSizeWarningLimit: 1000,
   },
-
 });
